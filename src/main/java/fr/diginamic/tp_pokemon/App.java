ae3c1e7e86c82117c7a542e0082b3004779e0b78
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-07-10 15:32:01 +0200

    start

---
+++ src/main/java/fr/diginamic/tp_pokemon/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.tp_pokemon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
